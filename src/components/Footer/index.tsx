038d01b89a278ca09772c601c89d36698c0ff04f
Prasanta Kakati <prasantakakati1994@gmail.com>
2023-09-02 00:36:02 +0530

    added repo link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,28 @@
+import { GitHub } from "react-feather";
+
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 flex h-16 w-full items-center justify-center">
+    <footer className="absolute bottom-0 flex h-16 w-full items-center justify-between px-4">
       <p>
         Designed and developed by{" "}
-        <a href="https://github.com/nullc0der" className="underline">
+        <a
+          href="https://github.com/nullc0der"
+          className="text-blue-600 underline"
+        >
           Prasanta Kakati
-        </a>
+        </a>{" "}
       </p>
+      <div className="flex items-center">
+        <GitHub size={14} />
+        <a
+          href="https://github.com/nullc0der/flexinotes"
+          className="ml-2 text-blue-600 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </div>
     </footer>
   );
 }
